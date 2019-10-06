e2ebfe7f082a08ed73e194a22657e2e1e564e9e9
shebetov <flex210655@gmail.com>
2019-10-06 15:02:07 +0300

    make it better

--- ncommon.py
+++ ncommon.py
@@ -12,20 +12,20 @@ def prepare_message(bytes_data):
 
 def send_message(to_socket, bytes_data):
     msg = prepare_message(bytes_data)
-    logger.info(f'> {time.time()} {msg}')
+    logger.info(f'> {time.time()} {len(msg)} {msg}')
     to_socket.send(msg)
 
 
 def receive_message(from_socket):
     try:
         message_header = from_socket.recv(HEADER_LENGTH)
-        logger.debug(f"< {time.time()} {message_header}")
+        logger.debug(f"< {time.time()} {len(message_header)} {message_header}")
         if not len(message_header):
             return False
 
         message_length = int(message_header.decode('utf-8').strip())
         message_data = from_socket.recv(message_length)
-        logger.debug(f"< {time.time()} {message_data}")
+        logger.debug(f"< {time.time()} {len(message_data)} {message_data}")
         return message_data
     except:
         return False
