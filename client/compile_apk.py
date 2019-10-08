43a0a3deedab9d72795488ee05dcb63fa2486443
Trixter <flex210655@gmail.com>
2019-10-08 21:15:10 +0300

    make it better

--- client/compile_apk.py
+++ client/compile_apk.py
@@ -1,3 +1,3 @@
 
 
-p4a apk --requirements=pyaudio --private /home/username/devel/planewave_frozen/ --package=net.inclem.planewavessdl2 --name="planewavessdl2" --version=0.5 --bootstrap=sdl2
+p4a apk --requirements=pyaudio --private /home/alex/telepathy/client/ --package=net.inclem.planewavessdl2 --name="planewavessdl2" --version=0.5 --bootstrap=sdl2
